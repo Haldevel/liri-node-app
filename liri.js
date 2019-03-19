aee0da2bcd181aca050399378f974e20b4b2fb4b
Susie <45341415+Haldevel@users.noreply.github.com>
2019-03-18 19:41:09 -0700

    add fourth selection logic

--- liri.js
+++ liri.js
@@ -35,14 +35,16 @@ inquirer
                 },
             ])
                 .then(function (inquirerResponse) {
-                  
+
                     console.log("Spotifying Song " + inquirerResponse.song);
 
                     //connect to spotify api and get the data for 3 top songs
 
-                    var spotify = new Spotify(keys.spotify);  
-                    
-                    //get three songs
+                    //var spotify = new Spotify(keys.spotify);
+
+                    runSpotify(inquirerResponse.song);
+
+                    /* //get three songs
                     spotify.search({ type: 'track', query: inquirerResponse.song, limit: 3 }, function (err, data) {
                         if (err) {
                             return console.log('Error occurred: ' + err);
@@ -57,8 +59,8 @@ inquirer
                             console.log("Album: " + JSON.stringify(data.tracks.items[i].album.name, null, 2));
                             console.log("--------------------------------------------------------------------------------");
                         }
-                    });
-                     
+                    }); */
+
                 });
 
         }
@@ -66,13 +68,61 @@ inquirer
             console.log("movie-this")
         }
         else if (inquirerResponse.actions === "do-what-it-says") {
-            console.log("do what?")
+            // import fs
+            var fs = require("fs");
+            var randomInput;
+
+            // we will read from randon.txt
+
+            fs.readFile("random.txt", "utf8", function (error, data) {
+
+                // If the code experiences any errors it will log the error to the console.
+                if (error) {
+                    return console.log(error);
+                }
+
+                // We will then print the contents of data
+                console.log(data);
+
+                // Then split it by commas (to make it more readable)
+                var dataArr = data.split(",");
+
+                runSpotify(dataArr[1]);
+
+                /* for(var i = 0; i < dataArr.length; i++) {
+                    var separ = dataArr[i].split(",");
+                    if(separ[0] === "spotify-this-song") {
+                        randomInput = separ[1];
+                    }
+                }
+                */
+
+            });
+
         }
     });
 
 
 
+function runSpotify(songName) {
+    var spotify = new Spotify(keys.spotify);
+    //get three songs
+    spotify.search({ type: 'track', query: songName, limit: 3 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
+        //retrieve data for 3 songs
+        for (var i = 0; i < 3; i++) {
+            console.log("--------------------------------------------------------------------------------");
+            console.log("Artist: " + JSON.stringify(data.tracks.items[i].album.artists[0].name, null, 2));
+            console.log("Name: " + JSON.stringify(data.tracks.items[i].name, null, 2));
+            console.log("Preview URL: " + JSON.stringify(data.tracks.items[i].preview_url, null, 2));
+            console.log("Album: " + JSON.stringify(data.tracks.items[i].album.name, null, 2));
+            console.log("--------------------------------------------------------------------------------");
+        }
+    });
+}
 
 
 
