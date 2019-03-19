4e9e15013812eb8b98fe23411c6294cd858eb647
Susie <45341415+Haldevel@users.noreply.github.com>
2019-03-18 19:21:32 -0700

    add default to spotify

--- liri.js
+++ liri.js
@@ -19,40 +19,59 @@ inquirer
         },
     ])
     .then(function (inquirerResponse) {
-        if(inquirerResponse.actions === "concert-this") {
+        if (inquirerResponse.actions === "concert-this") {
             console.log("bands")
         }
-        else if(inquirerResponse.actions === "spotify-this-song") {
-            console.log("spotify")
+        else if (inquirerResponse.actions === "spotify-this-song") {
+            console.log("spotify");
+            //get a song name from a user
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What song do you want to spotify?",
+                    name: "song",
+                    default: "The Sign Ace of Base"
+
+                },
+            ])
+                .then(function (inquirerResponse) {
+                  
+                    console.log("Spotifying Song " + inquirerResponse.song);
+
+                    //connect to spotify api and get the data for 3 top songs
+
+                    var spotify = new Spotify(keys.spotify);  
+                    
+                    //get three songs
+                    spotify.search({ type: 'track', query: inquirerResponse.song, limit: 3 }, function (err, data) {
+                        if (err) {
+                            return console.log('Error occurred: ' + err);
+                        }
+                    
+                        //retrieve data for 3 songs
+                        for (var i = 0; i < 3; i++) {
+                            console.log("--------------------------------------------------------------------------------");
+                            console.log("Artist: " + JSON.stringify(data.tracks.items[i].album.artists[0].name, null, 2));
+                            console.log("Name: " + JSON.stringify(data.tracks.items[i].name, null, 2));
+                            console.log("Preview URL: " + JSON.stringify(data.tracks.items[i].preview_url, null, 2));
+                            console.log("Album: " + JSON.stringify(data.tracks.items[i].album.name, null, 2));
+                            console.log("--------------------------------------------------------------------------------");
+                        }
+                    });
+                     
+                });
+
         }
-        else if(inquirerResponse.actions === "movie-this") {
+        else if (inquirerResponse.actions === "movie-this") {
             console.log("movie-this")
         }
-        else if(inquirerResponse.actions === "do-what-it-says") {
+        else if (inquirerResponse.actions === "do-what-it-says") {
             console.log("do what?")
         }
     });
 
 
 
-/* var spotify = new Spotify(keys.spotify);
-
-
-//get three songs
-spotify.search({ type: 'track', query: 'Vogue', limit: 3 }, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-
-    //retrieve data for 3 songs
-    for (var i = 0; i < 3; i++) {
-        console.log("Artist: " + JSON.stringify(data.tracks.items[i].album.artists[0].name, null, 2));
-        console.log("Name: " + JSON.stringify(data.tracks.items[i].name, null, 2));
-        console.log("Preview URL: " + JSON.stringify(data.tracks.items[i].preview_url, null, 2));
-        console.log("Album: " + JSON.stringify(data.tracks.items[i].album.name, null, 2));
-    }
-});
- */
 
 
 
