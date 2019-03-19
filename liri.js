537cebb54d53b5f2141644087ebfd2a7703ceaf3
Susie <45341415+Haldevel@users.noreply.github.com>
2019-03-18 20:37:10 -0700

    start working on IMDB

--- liri.js
+++ liri.js
@@ -5,6 +5,9 @@ var Spotify = require('node-spotify-api');
 // Load the NPM Package inquirer
 var inquirer = require("inquirer");
 
+// Include the axios package
+var axios = require("axios");
+
 var keys = require("./keys.js");
 
 // Create a "Prompt" with a series of questions.
@@ -45,23 +48,42 @@ inquirer
         else if (inquirerResponse.actions === "movie-this") {
             console.log("movie-this");
 
-            // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
-            var axios = require("axios");
+            //Retrive the movie name via prompt here
+
 
             // Then run a request with axios to the OMDB API with the movie specified
             axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy")
                 .then(
                     function (response) {
-                        console.log("The movie's rating is: " + response.data.imdbRating);
+                        console.log("--------------------------------------------------------------------------------");
+                        console.log("Title: "+ response.data.Title);
+                        console.log("Release Year: "+ response.data.Released);
+                        console.log("Country: "+ response.data.Country);
+                        console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+                        console.log("The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value);
+                        console.log("Language: " + response.data.Language);
+                        console.log("Plot: " + response.data.Plot);
+                        console.log("Actors: " + response.data.Actors);
+                        console.log("--------------------------------------------------------------------------------");
+                        //console.log(response.data);
+
                     }
                 );
 
-
+           /*      * Title of the movie.
+                * Year the movie came out.//
+                * IMDB Rating of the movie.//
+                * Rotten Tomatoes Rating of the movie.//
+                * Country where the movie was produced.//
+                * Language of the movie.
+                * Plot of the movie.
+                * Actors in the movie. */
+              
         }
         else if (inquirerResponse.actions === "do-what-it-says") {
             // import fs
             var fs = require("fs");
-            var randomInput;
+            //var randomInput;
 
             // we will read from randon.txt
             fs.readFile("random.txt", "utf8", function (error, data) {
@@ -104,7 +126,7 @@ function runSpotify(songName) {
         //retrieve data for 3 songs
         for (var i = 0; i < 3; i++) {
             console.log("--------------------------------------------------------------------------------");
-            console.log("Song # " + (i+1));
+            console.log("Song # " + (i + 1));
             console.log("--------------------------------------------------------------------------------");
             console.log("Artist: " + JSON.stringify(data.tracks.items[i].album.artists[0].name, null, 2));
             console.log("Name: " + JSON.stringify(data.tracks.items[i].name, null, 2));
