56435b13bcb3de3781f040160d9fd418c95348f1
Susie <45341415+Haldevel@users.noreply.github.com>
2019-03-20 18:17:18 -0700

    add error to axios call

--- liri.js
+++ liri.js
@@ -1,14 +1,13 @@
+//import all required packages/modules
+
 require("dotenv").config();
 
 var Spotify = require('node-spotify-api');
-
 var inquirer = require("inquirer");
-
 var axios = require("axios");
-
 var keys = require("./keys.js");
-
 var moment = require('moment');
+var fs = require("fs");
 
 // Create a Prompt with a list of options
 inquirer
@@ -22,34 +21,35 @@ inquirer
         },
     ])
     .then(function (inquirerResponse) {
-        if (inquirerResponse.actions === "concert-this") { //if the concert-this option was picked
-            console.log("bands");
+        if (inquirerResponse.actions === "concert-this") { //if the concert-this option was picked ask for performer
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Who is the performer/band you want to search for concerts of?",
+                    name: "performer",
+                    default: "Pink Martini"   //use Pink martini as default
 
-            axios.get("https://rest.bandsintown.com/artists/pink+martini/events?app_id=codingbootcamp")
-                .then(
-                    function (response) {
+                },
+            ])
+                .then(function (inquirerResponse) {
+                    //when user inputs data call the giveConcerts function to call bands in town api via axios
+                    var performer = inquirerResponse.performer;
+                    console.log("Searching for " + performer + " concerts");
+                    giveConcerts(performer);
 
-                        for (var i = 0; i < 10; i++) {
-                            console.log("--------------------------------------------------------------------------------");
-                            console.log("Performer: " + response.data[i].lineup[0]);
-                            console.log("Venue Name: " + response.data[i].venue.name);
-                            console.log("Venue Location: " + response.data[i].venue.city + " " + response.data[i].venue.region + " " + response.data[i].venue.country);
-                            console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
-                            console.log("--------------------------------------------------------------------------------");
-                        }
+                });
 
-                    }
-                );
+
         }
         else if (inquirerResponse.actions === "spotify-this-song") { //if spotify-this-son option is picked
-            console.log("spotify");
+
             //get a song name from a user
             inquirer.prompt([
                 {
                     type: "input",
                     message: "What song do you want to spotify?",
                     name: "song",
-                    default: "The Sign Ace of Base"
+                    default: "The Sign Ace of Base" //use the song as default
 
                 },
             ])
@@ -78,15 +78,13 @@ inquirer
                     var movieTitle = inquirerResponse.movie;
                     console.log("Getting info about " + movieTitle);
                     //call the function which gets movie info via axios
-                    checkIMDB(movieTitle);
+                    checkOMDB(movieTitle);
                 });
 
         }
         else if (inquirerResponse.actions === "do-what-it-says") {
-            // import fs
-            var fs = require("fs");
 
-            // we will read from randon.txt
+            //we will read the song's title from random.txt
             fs.readFile("random.txt", "utf8", function (error, data) {
 
                 // If the code experiences any errors it will log the error to the console.
@@ -94,34 +92,22 @@ inquirer
                     return console.log(error);
                 }
 
-                // We will then print the contents of data
-                console.log(data);
-
-                // Then split it by commas (to make it more readable)
+                // Split the data by commas
                 var dataArr = data.split(",");
 
-                runSpotify(dataArr[1]);
-
-                /* for(var i = 0; i < dataArr.length; i++) {
-                    var separ = dataArr[i].split(",");
-                    if(separ[0] === "spotify-this-song") {
-                        randomInput = separ[1];
-                    }
-                }
-                */
-
+                runSpotify(dataArr[1]); //calls the function to use spotify api and passes the song's title
             });
         }
     });
 
 
-
+//function to get data abou a song based on its title using node-Spotif-api
 function runSpotify(songName) {
     var spotify = new Spotify(keys.spotify);
-    //get three songs
+    //we will limit the output to three songs
     spotify.search({ type: 'track', query: songName, limit: 3 }, function (err, data) {
         if (err) {
-            return console.log('Error occurred: ' + err);
+            return console.log('Error occurred when using Spotify: ' + err);
         }
 
         //retrieve data for 3 songs
@@ -129,18 +115,18 @@ function runSpotify(songName) {
             console.log("--------------------------------------------------------------------------------");
             console.log("Song # " + (i + 1));
             console.log("--------------------------------------------------------------------------------");
-            console.log("Artist: " + JSON.stringify(data.tracks.items[i].album.artists[0].name, null, 2));
-            console.log("Name: " + JSON.stringify(data.tracks.items[i].name, null, 2));
-            console.log("Preview URL: " + JSON.stringify(data.tracks.items[i].preview_url, null, 2));
-            console.log("Album: " + JSON.stringify(data.tracks.items[i].album.name, null, 2));
+            console.log("Artist: " + data.tracks.items[i].album.artists[0].name);
+            console.log("Name: " + data.tracks.items[i].name);
+            console.log("Preview URL: " + data.tracks.items[i].preview_url);
+            console.log("Album: " + data.tracks.items[i].album.name);
             console.log("--------------------------------------------------------------------------------");
         }
     });
 }
 
-
-function checkIMDB(title) {
-    // Then run a request with axios to the IMDB API with the movie specified
+//function to get movie data using OMDB api and axios
+function checkOMDB(title) {
+    // Then run a request with axios to the OMDB API with the movie specified
     axios.get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy")
         .then(
             function (response) {
@@ -156,7 +142,60 @@ function checkIMDB(title) {
                 console.log("--------------------------------------------------------------------------------");
 
             }
-        );
+        )
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+
+
+}
+
+//function to look for concerts using bandsintown api and axios
+function giveConcerts(performer) {
+    axios.get("https://rest.bandsintown.com/artists/" + performer + "/events?app_id=codingbootcamp")
+        .then(
+            function (response) {
+                for (var i = 0; i < 10; i++) {
+                    console.log("--------------------------------------------------------------------------------");
+                    console.log("Performer: " + response.data[i].lineup[0]);
+                    console.log("Venue Name: " + response.data[i].venue.name);
+                    console.log("Venue Location: " + response.data[i].venue.city + " " + response.data[i].venue.region + " " + response.data[i].venue.country);
+                    console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                    console.log("--------------------------------------------------------------------------------");
+                }
+            }
+        )
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 
 }
 
